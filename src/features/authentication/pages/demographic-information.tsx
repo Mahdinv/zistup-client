6cecade7eb876bde89cda341e83560fb0bcfea77
Mahdi Navardi <s.mahdinavardi@gmail.com>
2026-07-21 12:00:21 +0330

    demographic-information completed. All remain is send requests to server.

--- src/features/authentication/pages/demographic-information.tsx
+++ src/features/authentication/pages/demographic-information.tsx
@@ -9,16 +9,28 @@ import NumberPicker from "../../../shared/base-components/number-picker";
 import RulerBox from "../../../shared/base-components/ruler-box";
 import { useOutletContext } from "react-router-dom";
 import type { AuthOutletContext } from "../../../layouts/auth-layout";
+import {
+  Controller,
+  useForm,
+  type FieldPath,
+  type SubmitHandler,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DemographicInformationSchema,
+  type DemographicInformation,
+} from "../schemas/demographic-information.schema";
+import CheckBox from "../../../shared/base-components/check-box";
 
 const genderOptions = [
   {
     title: "آقا",
-    value: "male",
+    value: 1,
     icon: <LiaMaleSolid className="text-2xl" strokeWidth={0.8} />,
   },
   {
     title: "خانم",
-    value: "female",
+    value: 2,
     icon: <LiaFemaleSolid className="text-2xl" strokeWidth={0.8} />,
   },
 ];
@@ -36,11 +48,36 @@ const focusOptions = [
   { title: "سایر...", value: "others" },
 ];
 
+const stepOneFields: FieldPath<DemographicInformation>[] = [
+  "name",
+  "sex",
+  "weight",
+  "height",
+  "age",
+];
+
 const DemographicInformation = () => {
   const { setBackHandler } = useOutletContext<AuthOutletContext>();
   const [step, setStep] = useState<1 | 2>(1);
-  const [number, setNumber] = useState(20);
-  const [height, setHeight] = useState(150);
+
+  const {
+    register,
+    control,
+    trigger,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DemographicInformation>({
+    resolver: zodResolver(DemographicInformationSchema),
+    defaultValues: {
+      name: "",
+      sex: undefined,
+      weight: 70,
+      height: 100,
+      age: 0,
+      mainGoal: "",
+      focus: [],
+    },
+  });
 
   useEffect(() => {
     if (step === 2) {
@@ -53,6 +90,19 @@ const DemographicInformation = () => {
       setBackHandler(null);
     };
   }, [step, setBackHandler]);
+
+  const onNextStepHandler = async () => {
+    const isStepOneValid = await trigger(stepOneFields, {
+      shouldFocus: true,
+    });
+    if (!isStepOneValid) return;
+
+    setStep(2);
+  };
+
+  const onDemographicInformationFormHandler: SubmitHandler<
+    DemographicInformation
+  > = () => {};
 
   return (
     <MainContainer isFirstStep={step === 1} isLastStep={step === 2}>
@@ -97,52 +147,79 @@ const DemographicInformation = () => {
               inlineLabel
               label="نام و نشان"
               placeHolder="مثال: پارسا متینی"
+              {...register("name")}
+              error={errors.name?.message}
             />
-            <Radio
-              inlineLabel
-              label="جنسیت"
-              gridClasses="grid-cols-2 gap-1"
-              options={genderOptions}
-              value="male"
-              onChange={() => {}}
+            <Controller
+              name="sex"
+              control={control}
+              render={({ field }) => (
+                <Radio
+                  inlineLabel
+                  label="جنسیت"
+                  gridClasses="grid-cols-2 gap-1"
+                  options={genderOptions}
+                  value={field.value}
+                  onChange={field.onChange}
+                  error={errors.sex?.message}
+                />
+              )}
             />
-            <NumberPicker
-              label="وزن"
-              subLabel="(کیلوگرم)"
-              min={0}
-              max={200}
-              value={number}
-              onChangeEnd={setNumber}
+            <Controller
+              name="weight"
+              control={control}
+              render={({ field }) => (
+                <NumberPicker
+                  label="وزن"
+                  subLabel="(کیلوگرم)"
+                  min={0}
+                  max={200}
+                  value={field.value}
+                  onChangeEnd={field.onChange}
+                  error={errors.weight?.message}
+                />
+              )}
             />
-            <RulerBox
-              label="قد"
-              subLabel="(سانتی متر)"
-              min={150}
-              max={210}
-              step={1}
-              majorStep={5}
-              value={height}
-              onChange={setHeight}
-              onChangeEnd={(finalHeight) => {
-                console.log("قد نهایی انتخاب‌شده:", finalHeight);
-              }}
+            <Controller
+              name="height"
+              control={control}
+              render={({ field }) => (
+                <RulerBox
+                  label="قد"
+                  subLabel="(سانتی متر)"
+                  min={100}
+                  max={220}
+                  step={1}
+                  majorStep={5}
+                  value={field.value}
+                  onChangeEnd={field.onChange}
+                  error={errors.height?.message}
+                />
+              )}
             />
-            <NumberPicker
-              label="سن"
-              min={0}
-              max={100}
-              value={number}
-              onChangeEnd={setNumber}
+            <Controller
+              name="age"
+              control={control}
+              render={({ field }) => (
+                <NumberPicker
+                  label="سن"
+                  min={0}
+                  max={100}
+                  value={field.value}
+                  onChangeEnd={field.onChange}
+                  error={errors.age?.message}
+                />
+              )}
             />
           </div>
           <Button
             classes="btn btn-primary"
             title="مرحله بعد"
-            onClick={() => setStep(2)}
+            onClick={onNextStepHandler}
           />
         </motion.div>
       ) : (
-        <motion.div
+        <motion.form
           key="step-2"
           initial={{ x: -24, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
@@ -162,30 +239,46 @@ const DemographicInformation = () => {
                     compact:px-4
                     mobile-lg:px-6
                     will-change-[transform,opacity]"
+          onSubmit={handleSubmit(onDemographicInformationFormHandler)}
         >
-          <Radio
-            label="هدف اصلی تو چیه؟"
-            subLabel="(فقط یک انتخاب)"
-            gridClasses="grid-cols-3 gap-1"
-            options={mainGoalOptions}
-            value="weight-gain"
-            onChange={() => {}}
+          <Controller
+            name="mainGoal"
+            control={control}
+            render={({ field }) => (
+              <Radio
+                label="هدف اصلی تو چیه؟"
+                subLabel="(فقط یک انتخاب)"
+                gridClasses="grid-cols-3 gap-1"
+                options={mainGoalOptions}
+                value={field.value}
+                onChange={field.onChange}
+                error={errors.mainGoal?.message}
+              />
+            )}
           />
-          <Radio
-            label="دوست داری روی چیا بیشتر تمرکز کنیم؟"
-            gridClasses="grid-cols-3 gap-1"
-            options={focusOptions}
-            value="environment"
-            onChange={() => {}}
+
+          <Controller
+            name="focus"
+            control={control}
+            render={({ field }) => (
+              <CheckBox
+                label="دوست داری روی چیا بیشتر تمرکز کنیم؟"
+                gridClasses="grid-cols-3 gap-1"
+                options={focusOptions}
+                values={field.value}
+                onChange={field.onChange}
+                error={errors.focus?.message}
+              />
+            )}
           />
           <div className="w-full h-full flex flex-col justify-end">
             <Button
+              type="submit"
               classes="btn btn-primary"
               title="ثبت اطلاعات"
-              onClick={() => setStep(1)}
             />
           </div>
-        </motion.div>
+        </motion.form>
       )}
     </MainContainer>
   );
