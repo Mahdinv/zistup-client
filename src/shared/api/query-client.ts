0ce1e8acb7452d8d17d8d5c1a7093028cc745ab7
Mahdi Navardi <s.mahdinavardi@gmail.com>
2026-08-20 21:27:23 +0330

    clear bad comments

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -6,17 +6,12 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: shouldRetryQuery,
-
       retryDelay: (attemptIndex) => {
         return Math.min(1_000 * 2 ** attemptIndex, 10_000);
       },
     },
 
     mutations: {
-      /**
-       * POST، PATCH و DELETE را
-       * خودکار تکرار نمی‌کنیم.
-       */
       retry: false,
     },
   },
